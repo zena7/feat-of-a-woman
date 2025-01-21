050afe73ced7b1f7262e114bec8593471bd181f2
Nataly Zernova <nz_18@mail.ru>
2025-01-21 23:58:25 +0300

    refactor: set routes in app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
-import './index.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import App from './App.jsx';
-import Welcome from './Welcome.jsx';
-import Warning from './Warning.jsx';
+import './index.css';
+import Welcome from './pages/welcome/Welcome.jsx';
+import Warning from './pages/warning/Warning.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
