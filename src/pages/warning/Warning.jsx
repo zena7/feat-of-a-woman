1713c891364fdcc3572a4d0f290f09c37760b31a
Nataly Zernova <nz_18@mail.ru>
2025-01-23 20:46:20 +0300

    refactor: alter layout with css styles and create opening translating block

--- src/pages/warning/Warning.jsx
+++ src/pages/warning/Warning.jsx
@@ -1,32 +1,67 @@
-import { Link } from 'react-router';
-import aggressiveHippoVideo from '../../../public/assets/aggressive-hippo.mp4';
+import { useState, useCallback } from 'react';
+import { useNavigate } from 'react-router';
+import aggressiveHippoVideo from '../../../src/assets/aggressive-hippo.mp4';
 import styles from './Warning.module.css';
+import clsx from 'clsx';
 
 function Warning() {
-  const handleClick = () => {
-    console.log('CLICK');
-  };
+  const [showRu, setShowRu] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClick = useCallback(() => {
+    navigate('/game');
+  }, [navigate]);
+
+  const handleClickShowRuText = useCallback(() => {
+    setShowRu((prev) => !prev);
+  }, []);
 
   return (
     <>
       <video autoPlay muted loop className={styles['background-video']}>
         <source src={aggressiveHippoVideo} type="video/mp4" />
       </video>
-      <div className={styles.content}>
-        <h1>Dangerous Hippo</h1>
-        <p>
-          По данным Всемирной организации здравоохранения (ВОЗ) и других
-          источников, бегемоты ежегодно убивают несколько сотен человек в
-          Африке. Это делает их одними из наиболее смертоносных крупных животных
-          на континенте.
-        </p>
-        <p>
-          Бегемоты могут проявлять агрессию, если напугаются. Бегемот может
-          случайно задавить человека, если неожиданно переместится в сторону.
-        </p>
-        <button onClick={handleClick}>Start</button>
-        <Link to="/game">Next</Link> {/*сделать через кнопку and useNavigate*/}
-      </div>
+      <section className={styles.content}>
+        <div className="enContent">
+          <h1 className={styles.header}>Dangerous Hippo</h1>
+          <p className={styles.firstP}>
+            According to the World Health Organization, hippos kill hundreds of
+            people every year in Africa. This makes them one of the most
+            dangerous big animals there.
+          </p>
+          <p>
+            {' '}
+            Hippos can exhibit aggression if they are scared. They might
+            accidentally hurt someone if they move suddenly.
+          </p>
+        </div>
+        <div className={styles.btnWrapper}>
+          <button onClick={handleClickShowRuText} className={styles.btn}>
+            {!showRu ? 'Show translate' : 'Hide'}
+          </button>
+          <button
+            type="button"
+            className={clsx(styles.btn, styles.btnDangerNext)}
+            onClick={handleClick}
+          >
+            Next
+          </button>
+        </div>
+
+        {showRu && (
+          <div className={styles.ruContent}>
+            <p className={styles.firstP}>
+              По данным Всемирной организации здравоохранения, бегемоты ежегодно
+              убивают несколько сотен человек в Африке. Это делает их одними из
+              наиболее смертоносных крупных животных на континенте.
+            </p>
+            <p>
+              Бегемоты могут проявлять агрессию, если они напугаются. Бегемот
+              может случайно задавить человека, переместившись в сторону.
+            </p>
+          </div>
+        )}
+      </section>
     </>
   );
 }
