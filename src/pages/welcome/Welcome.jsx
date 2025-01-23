39b6a7498f12f42882f8fef9221b276eef93bb51
Nataly Zernova <nz_18@mail.ru>
2025-01-23 16:55:47 +0300

    style: add styles for layout in welcome page

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -1,8 +1,7 @@
 import clsx from 'clsx';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import cityImg from '../../../public/assets/leningrad.webp';
-import Container from '../../components/container/Сontainer';
+import cityImg from '../../../public/assets/welcomeLeningrad.webp';
 import styles from './Welcome.module.css';
 
 function Welcome() {
@@ -16,22 +15,28 @@ function Welcome() {
     setTimeout(() => {
       navigate('/warning');
     }, 5000);
-    // navigate('/warning');
   };
 
   return (
     <>
-      <Container>
-        <h1 className={clsx(showHeader ? styles.itemShow : styles.itemHidden)}>
-          Leningrad <br /> Saint Petersburg
-        </h1>
-        <img src={cityImg} alt="leningrad" />
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="">In which city is the zoo located?</label>
-          <input type="text" />
-          <button type="submit">Next</button>
-        </form>
-      </Container>
+      <h1
+        className={clsx(
+          styles.cityHeader,
+          showHeader ? styles.itemShow : styles.itemHidden
+        )}
+      >
+        Leningrad <br /> Saint Petersburg
+      </h1>
+      <img src={cityImg} alt="leningrad" className={styles.welcomeImg} />
+      <form onSubmit={handleSubmit} className={styles.welcomeForm}>
+        <label htmlFor="city" className={styles.formLabel}>
+          In which city is the zoo located?
+        </label>
+        <input type="text" id="city" className={styles.inputCity} />
+        <button type="submit" className={styles.btnCity}>
+          Next
+        </button>
+      </form>
     </>
   );
 }
