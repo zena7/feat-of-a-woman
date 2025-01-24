877b25bdb85c1abc098045659ed938daf180ec17
Nataly Zernova <nz_18@mail.ru>
2025-01-24 20:12:24 +0300

    chore: add normalize for project

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import App from './App.jsx';
+import 'normalize.css';
 import './index.css';
 import Welcome from './pages/welcome/Welcome.jsx';
 import Warning from './pages/warning/Warning.jsx';
